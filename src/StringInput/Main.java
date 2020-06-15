58a71fd0bcda28f201f9f7457dc048d72034325a
Jihun Park <jxxparker@gmail.com>
2020-06-15 11:28:48 -0500

    scanner

---
+++ src/StringInput/Main.java
@@ -0,0 +1,18 @@
+package StringInput;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("enter pokemon 1:");
+		String pokemon1 = scan.nextLine();
+		
+		System.out.println(pokemon1);
+		
+	}
+
+}
