6fb707ca42d4ff01c9da537ef48d8cfb42e3e644
Jihun Park <jxxparker@gmail.com>
2020-06-15 14:41:37 -0500

    more stuff

---
+++ src/String/Main.java
@@ -0,0 +1,12 @@
+package String;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("madlibs");
+		
+		String username = "jxxparker";
+		
+	}
+
+}
---
+++ src/StringMethods/Main.java
@@ -0,0 +1,13 @@
+package StringMethods;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String name = "jihun park is my name";
+		
+		System.out.println("name: " + name);
+		System.out.println(name.charAt(0));
+	}
+
+}
